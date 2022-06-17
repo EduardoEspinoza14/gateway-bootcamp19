88d03f08bc5b542f8637235445df90486c9cd4ec
EduardoEspinoza14 <eduardoantonio.espinozallontop@everis.nttdata.com>
2022-06-17 10:13:20 -0500

    Commit

--- src/main/java/com/nttdata/gateway/logging/LoggingFilter.java
+++ src/main/java/com/nttdata/gateway/logging/LoggingFilter.java
@@ -1,5 +1,13 @@
 package com.nttdata.gateway.logging;
 
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -9,14 +17,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.util.Collections;
-import java.util.Set;
-
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR;
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
-
 @Component
 public class LoggingFilter implements GlobalFilter {
 
@@ -24,11 +24,14 @@ public class LoggingFilter implements GlobalFilter {
 
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-    Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+    Set<URI> uris = exchange
+            .getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
     String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
-    Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
     URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
-    log.info("Incoming request " + originalUri + " is routed to id: " + route.getId() + ", uri:" + routeUri);
+    Optional.ofNullable(exchange.getAttribute(GATEWAY_ROUTE_ATTR))
+            .map(Route.class::cast)
+            .ifPresent(route -> log.info("Incoming request " + originalUri + " is routed to id: "
+              + route.getId() + ", uri:" + routeUri));
     return chain.filter(exchange);
   }
 }
